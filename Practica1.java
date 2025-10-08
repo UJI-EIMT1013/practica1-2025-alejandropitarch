7200eb68cfeea7fffa4fd9a8ae3ab222c347a003
alejandropitarch <al448002@uji.es>
2025-10-08 12:09:56 +0200

    Ejercicio 1 OK, EJs. 2 y 3 REVISAR

--- Practica1.java
+++ Practica1.java
@@ -6,20 +6,85 @@ public class Practica1 {
 
     //EJERCICIO 1
     public static Set<Integer> multiplos (Iterator<Integer> it) {
-        //TODO
-        return null;
+        Set<Integer> vistos = new HashSet<>(), resultado = new HashSet<>();
+        while (it.hasNext()) {
+            int iIni = it.next();
+            if (iIni != 0){
+                Iterator<Integer> itVistos = vistos.iterator();
+
+                while (itVistos.hasNext()) {
+                    int visto = itVistos.next();
+                    if (visto % iIni == 0) {
+                        resultado.add(visto);
+                    } else if (iIni % visto == 0) {
+                        resultado.add(iIni);
+                    }
+                } vistos.add(iIni);
+            }
+        }
+        return resultado;
     }
 
-    //EJERCICIO2
+    //EJERCICIO 2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+        Set<Integer> todos = new HashSet<>(cuadrados);
+        todos.addAll(noCuadrados);
+        Iterator<Integer> itTodos = todos.iterator();
 
+        while (itTodos.hasNext()){
+            int iTodos = itTodos.next();
+            Iterator<Integer> itCuad = cuadrados.iterator();
+            while (itCuad.hasNext()) {
+                int iCuad = itCuad.next();
+                boolean esCuadrado = false;
+                if (iTodos*iTodos == iCuad && iCuad != iTodos) {
+                    esCuadrado = true;
+                }
+                if (!esCuadrado) {
+                    itCuad.remove();
+                    noCuadrados.add(iCuad);
+                }
+            }
+            Iterator<Integer> itNoCuad = noCuadrados.iterator();
+            while (itNoCuad.hasNext()) {
+                int iNoCuad = itNoCuad.next();
+                boolean esCuadrado = false;
+                if (iNoCuad*iNoCuad == iTodos && iNoCuad != iTodos) {
+                    esCuadrado = true;
+                }
+                if (esCuadrado) {
+                    itNoCuad.remove();
+                    cuadrados.add(iNoCuad);
+                }
+            }
+        }
     }
 
     //EJERCICIO 3
     public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
-        //TODO
-        return null;
+        Collection<Set<T>> col = new ArrayList<>();
+        Iterator<Set<T>> itCol = col.iterator();
+        while (it.hasNext()) {
+            boolean contenido = false;
+            T elem = it.next();
+            while (itCol.hasNext()) {
+                Set<T> s = itCol.next();
+                if (!s.contains(elem)) {
+                    s.add(elem);
+                    contenido = true;
+                }
+            }
+            if (!contenido) {
+                Set<T> s = new HashSet<>();
+                s.add(elem);
+                col.add(s);
+            }
+        }
+
+
+
+
+        return col;
     }
 
     //EJERCICIO 4
